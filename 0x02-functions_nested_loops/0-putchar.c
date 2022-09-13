f7b58dde3d97d46ede2c523b6f2067bd7b1229b3
Caleb Atiemo-Keseku <calebatiemo13@gmail.com>
2022-09-13 15:52:17 -0700

    new files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - main block
+ *
+ * Description:Prints _putchar followed by
+ * a new line
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char p[8] = "_putchar";
+	int i = 0;
+	while (i < 9)
+	{
+		_putchar(p[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
