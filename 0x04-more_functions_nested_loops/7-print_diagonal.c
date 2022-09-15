871f07ae7481d8cd6d57c2eefe5bd6e14d1916d9
Caleb Atiemo-Keseku <calebatiemo13@gmail.com>
2022-09-15 14:02:37 -0700

    new files

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function
+ * Description: draws a diagonal on the terminal
+ * @n: number of \ to be printed
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0, a;
+
+	while (i < n && n > 0)
+	{
+		a = 0;
+		while (a < i)
+		{
+			_putchar(' ');
+			a++;
+		}
+		_putchar('\');
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
+}
