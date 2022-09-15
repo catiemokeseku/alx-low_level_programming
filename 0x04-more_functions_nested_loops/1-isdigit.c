7de0c689391e3c188a57ebf603204781e694a844
Caleb Atiemo-Keseku <calebatiemo13@gmail.com>
2022-09-15 10:31:35 -0700

    new files

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigt - function
+ *
+ * Description: function to check for digits
+ * @c: character to check
+ *
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 0 && c <= 9);
+}
