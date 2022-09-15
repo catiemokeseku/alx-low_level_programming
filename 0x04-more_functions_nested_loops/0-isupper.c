8adcf7d85c2f865211a9c7734baca8a473ade300
Caleb Atiemo-Keseku <calebatiemo13@gmail.com>
2022-09-15 10:21:42 -0700

    new files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - function
+ *
+ * Description: checks for uppercase character
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
