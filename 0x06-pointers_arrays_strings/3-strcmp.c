9cc0314ad206aed6788f154168c3871045c8522e
Caleb Atiemo-Keseku <calebatiemo13@gmail.com>
2022-09-22 10:05:43 -0700

    new files

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcmp - function
+ *
+ * Description: compares two strings
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ *
+ * Return: 0 if matching and ns1 - ns2 if not matching
+ */
+
+
