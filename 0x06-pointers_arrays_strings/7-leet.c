66901c55e9c15aff3af50e1c7b8f807c8b8831b3
catiemokeseku <43410683+catiemokeseku@users.noreply.github.com>
2022-09-23 21:05:23 +0000

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -16,12 +16,12 @@ char *leet(char *str)
 	char a[] = "aAeEoOtTlL";
 	char b[] = "4433007711";
 
-	for (i = 0; *(s + i); i++)
+	for (i = 0; *(str + i); i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (a[j] == *(s + i))
-				*(s + i) = b[j];
+			if (a[j] == *(str + i))
+				*(str + i) = b[j];
 		}
 	}
 	return (str);
