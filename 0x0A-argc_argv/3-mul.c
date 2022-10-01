57620e4b103fab75e2d8968dc2d6a796485b71b6
Caleb Atiemo-Keseku <calebatiemo13@gmail.com>
2022-10-01 03:18:05 -0700

    new file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - entry point
@@ -8,7 +9,7 @@
  * @argc: int
  * @argv: array
  *
- * Return: 0
+ * Return: 0 or 1
  */
 
 int main(int argc, char *argv)
@@ -16,10 +17,11 @@ int main(int argc, char *argv)
 	if (argc - 1 != 2)
 	{
 		printf("Error\n");
+		return (1);
 	}
 	else
 	{
-		printf("%d\n", argv[1] * argv[2]);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	}
 	return (0);
 }
