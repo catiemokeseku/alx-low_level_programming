053b93d4143650d355be21efd2c272a79a1d74b5
Caleb Atiemo-Keseku <calebatiemo13@gmail.com>
2022-10-01 02:33:07 -0700

    new files

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints number of arguments
+ *
+ * @argc: int
+ * @argv: array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+}
