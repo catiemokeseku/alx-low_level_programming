476e23975d25d572344b166c64462ab9514aa3d1
catiemokeseku <43410683+catiemokeseku@users.noreply.github.com>
2022-10-06 22:08:16 +0000

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	l = 0;
 
 	for (i = 0; s1[i]; i++)
-		c[i++] = s1[i];
+		c[l++] = s1[i];
 
 	for (i = 0; s2[i] && i < n; i++)
 		c[l++] = s2[i];
