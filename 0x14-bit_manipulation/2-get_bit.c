d9c735b38933f84ff89575ef9549479dee77cb8b
catiemokeseku <43410683+catiemokeseku@users.noreply.github.com>
2022-10-27 20:45:04 +0000

    Create 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_bit - function
+ *
+ * Description: gets bit on index `index`
+ *
+ * @n: decimal number
+ * @index: index of the bit
+ *
+ * Return: the bit found
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+  if (index >= sizeof(n) * BYTE_LENGHT)
+    return (-1);
+  return ((n >> index) & 1);
+}
