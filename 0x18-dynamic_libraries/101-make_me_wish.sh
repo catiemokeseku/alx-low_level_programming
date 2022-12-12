94d17a40323a0f76e284ddd594d3fc7f54b8798f
catiemokeseku <43410683+catiemokeseku@users.noreply.github.com>
2022-12-12 14:11:58 +0000

    Update 101-make_me_wish.sh

--- 0x18-dynamic_libraries/101-make_me_wish.sh
+++ 0x18-dynamic_libraries/101-make_me_wish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/catiemokeseku/alx-low_level_programming/raw/master/0x18-dynamic_libraries
+wget -P /tmp/ https://raw.github.com/catiemokeseku/alx-low_level_programming/master/0x18-dynamic_libraries
 export LD_PRELOAD=/tmp/number.so
